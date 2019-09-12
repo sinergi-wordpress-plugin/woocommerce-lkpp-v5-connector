5d3c39004ec3ec9db3420d91851ced5ea9bcad6e
Administrator <admin@spakat.id>
2019-09-12 10:57:48 +0700

    Update spakat-lkpp-connector.php

--- spakat-lkpp-connector.php
+++ spakat-lkpp-connector.php
@@ -7,20 +7,20 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://github.com/spakat-id
+ * @link              https://github.com/sinergi-wordpress-plugin
  * @since             0.0.1
- * @package           Spakat-lkpp-connector
+ * @package           woocommerce-lkpp-v5-connector
  *
  * @wordpress-plugin
- * Plugin Name:       Spakat LKPP Connector
- * Plugin URI:        https://github.com/spakat-id/spakat-lkpp-connnector
- * Description:       Woocommerce connector for LKPP E-Catalogue
+ * Plugin Name:       Woocommerce LKPP E-Catalogue V5 Connector
+ * Plugin URI:        https://github.com/sinergi-wordpress-plugin/woocommerce-lkpp-v5-connector
+ * Description:       Woocommerce connector for LKPP E-Catalogue V5
  * Version:           0.0.1
  * Author:            Sinergi Creative
- * Author URI:        https://github.com/spakat-id
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       spakat-lkpp-connector
+ * Author URI:        https://github.com/sinergi-wordpress-plugin
+ * License:           GPL-3.0
+ * License URI:       https://github.com/sinergi-wordpress-plugin/woocommerce-lkpp-v5-connector/blob/master/LICENSE
+ * Text Domain:       woocommerce-lkpp-v5-connector
  * Domain Path:       /languages
  *
  * WC requires at least: 3.5.0
@@ -61,8 +61,6 @@ if ( ! lkpp_is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
  * @since 0.0.1
  */
  
-add_action( 'init', 'create_lkpp_category_taxonomy', 0 );
- 
 function create_lkpp_category_taxonomy() {
  
     // Labels part for the GUI
@@ -84,14 +82,19 @@ function create_lkpp_category_taxonomy() {
         'menu_name' => __( 'LKPP Product Categories' ),
     ); 
  
-    // Now register the non-hierarchical taxonomy like tag
-    register_taxonomy('topics','post',array(
-        'hierarchical' => false,
-        'labels' => $labels,
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'update_count_callback' => '_update_post_term_count',
-        'query_var' => true,
-        'rewrite' => array( 'slug' => 'lkpp_product_category' ),
-  ));
+    // set taxonomy option data
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => false,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+    );
+
+    // register taxonomy to woocommerce product object
+    register_taxonomy( 'lkpp_product_category', 'product', $args );
+    register_taxonomy_for_object_type( 'lkpp_product_category', 'product' );
 }
+add_action( 'init', 'create_lkpp_category_taxonomy', 0 );
