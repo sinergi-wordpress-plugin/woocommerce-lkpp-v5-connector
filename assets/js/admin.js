6ee3d61d68382b7cead9a9e981de9a58594b8d12
Administrator <admin@spakat.id>
2019-09-30 15:30:20 +0700

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -78,4 +78,26 @@ jQuery(function($){
             $(".tkdn_field").show();
         }
     }).change();
+
+    // Calculate LKPP Disc Percentage when LKPP price changed
+    $("#lkpp_price").change(function() {
+
+        $lkpp_price_inc = $(this).val();
+        $web_price = $("#_regular_price").val();
+        $lkpp_price_exc = Math.round($lkpp_price_inc - ($lkpp_price_inc * (10/100)));
+        $lkpp_disc = 100 - Math.round(($lkpp_price_exc/$web_price) * 100);
+        document.getElementById('lkpp_disc').value = $lkpp_disc; 
+
+    }).change();
+
+    // Calculate LKPP Disc Percentage when Web price changed
+    $("#_regular_price").change(function() {
+
+        $web_price = $(this).val();
+        $lkpp_price_inc = $("#lkpp_price").val();
+        $lkpp_price_exc = Math.round($lkpp_price_inc - ($lkpp_price_inc * (10/100)));
+        $lkpp_disc = 100 - Math.round(($lkpp_price_exc/$web_price) * 100);
+        document.getElementById('lkpp_disc').value = $lkpp_disc; 
+
+    }).change();
 });
