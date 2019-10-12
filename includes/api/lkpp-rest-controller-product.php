cb85eb16befd2e301e5e2123866535c1e5a67e05
Administrator <admin@spakat.id>
2019-10-12 17:09:53 +0700

    update rest api

--- includes/api/lkpp-rest-controller-product.php
+++ includes/api/lkpp-rest-controller-product.php
@@ -22,6 +22,24 @@ function lkpp_authentication($secret){
 }
 
 /**
+ * Handle a custom 'lkpp_active' query var to get products with the 'lkpp_active' meta.
+ * @param array $query - Args for WP_Query.
+ * @param array $query_vars - Query vars from WC_Product_Query.
+ * @return array modified $query
+ */
+function lkpp_active_query_var( $query, $query_vars ) {
+	if ( ! empty( $query_vars['lkpp_active'] ) ) {
+		$query['meta_query'][] = array(
+			'key' => 'lkpp_active',
+			'value' => esc_attr( $query_vars['lkpp_active'] ),
+		);
+	}
+
+	return $query;
+}
+add_filter( 'woocommerce_product_data_store_cpt_get_products_query', 'lkpp_active_query_var', 10, 2 );
+
+/**
  * Rest api callback function
  * @param $request
  */
@@ -29,6 +47,9 @@ function get_all_lkpp_product($request){
 	
 	// Product query result list
 	$product_list = array();
+
+	// Get LKPP Settings
+	$settings = get_option('lkpp_settings');
 
 	// Product object data
 	$product = array(
@@ -39,7 +60,7 @@ function get_all_lkpp_product($request){
 			'no_produk_penyedia' => '',
 			'id_manufaktur' => 0,
 			'berlaku_sampai' => '',
-			'id_penawaran_lkpp' => 0,
+			'id_penawaran_lkpp' => (int)$settings['lkpp_id'],
 			'id_unit_pengukuran_lkpp' => 7,
 			'deskripsi_singkat' => '',
 			'deskripsi_lengkap' => '',
@@ -76,19 +97,63 @@ function get_all_lkpp_product($request){
 	);
 
 	// JSON response format
-	$json_template = array(
-		'total'			=> '',
-		'current_page'	=> '',
-		'per_page'		=> '',
-		'total_page'	=> '',
+	$json_response = array(
+		'total'			=> 0,
+		'current_page'	=> 1,
+		'per_page'		=> 500,
+		'total_page'	=> 0,
 		'produk'		=> array()
 	);
 
 	$params = $request->get_params();
 	$secret_key = sanitize_text_field($request->get_param( 'secretkey' ));
 	$authentication = lkpp_authentication($secret_key);
-	if($authentication['msg'] == 'error'){
+	if($authentication['act'] == 'error'){
 		return new WP_Error( 'invalid_secret', 'Invalid secret Key', array('status' => 403) );
+	} else {
+		$settings = get_option('lkpp_settings');
+		$args = array(
+			'lkpp_active'	=> 'active',
+			'paginate'		=> true,
+			'limit'			=> 500,
+			'page'			=> 1,
+			'orderby'		=> 'date',
+			'order'			=> 'DESC'
+		);
+		$data = wc_get_products($args);
+		foreach($data->products as $item){
+			$item_id = $item->get_id();
+			$item_term = get_the_terms( $item_id, 'product_cat' );
+			$lkpp_publish = get_post_meta($item_id, 'lkpp_publish', true);
+			$local_product = get_post_meta($item_id, 'local_product', true);
+			$product['informasi']['unspsc'] = (int)get_term_meta($item_term[0]->term_id,'unspsc_code',true);
+			$product['informasi']['id_kategori_produk_lkpp'] = (int)get_post_meta($item_id, 'lkpp_product_category_id', true);
+			$product['informasi']['nama_produk'] = $item->get_name();
+			$product['informasi']['no_produk_penyedia'] = $item->get_sku();
+			$product['informasi']['id_manufaktur'] = (int)get_post_meta($item_id, 'lkpp_brand_id', true);
+			$product['informasi']['berlaku_sampai'] = get_post_meta($item_id, 'lkpp_expired_date', true);
+			$product['informasi']['deskripsi_singkat'] = $item->get_short_description();
+			$product['informasi']['deskripsi_lengkap'] = $item->get_description();
+			$product['informasi']['kuantitas_stok'] = (int)get_post_meta($item_id, 'lkpp_stock', true);
+			$product['informasi']['tanggal_update'] = get_the_modified_date( 'Y-m-d H:i:s', $item_id );
+			$product['informasi']['tkdn_produk'] = get_post_meta($item_id, 'tkdn', true);
+			if($lkpp_publish == 'publish'){
+				$product['informasi']['produk_aktif'] = 1;
+			}
+			if($local_product == 'yes'){
+				$product['informasi']['apakah_produk_lokal'] = 1;
+			}
+			$product['informasi']['url_produk'] = get_permalink( $item_id );
+			array_push($product_list, $product);
+		}
+		$json_response['total'] = $data->total;
+		$json_response['total_page'] = $data->max_num_pages;
+		$json_response['produk'] = $product_list;
+		//$success = 'Connected';
+		$response = new WP_REST_Response($json_response);
+    	$response->set_status(200);
+
+    	return $response;	
 	}
 }
 
@@ -97,7 +162,10 @@ function get_all_lkpp_product($request){
  */
 add_action('rest_api_init', function () {
 	register_rest_route( 'lkpp/v1', 'all_produk',array(
-				  'methods'  => 'GET',
-				  'callback' => 'get_all_lkpp_product'
-		));
-  });
+		'methods'  => 'GET',
+		'callback' => 'get_all_lkpp_product',
+		'permission_callback' => function() {
+			return true;
+		}
+	));
+});
--- woocommerce-lkpp-v5-connector.php
+++ woocommerce-lkpp-v5-connector.php
@@ -248,11 +248,105 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
             update_post_meta($post->ID, 'lkpp_expired_date', $default_date);
             $lkpp_expired_date = $default_date;
         }
+
+        /*$inline =   '// multiple select with AJAX search
+                    $("#lkpp_product_category_id").select2({
+                        ajax: {
+                            url: ajaxurl, // AJAX URL is predefined in WordPress admin
+                            dataType: "json",
+                            delay: 250, // delay in ms while typing when to perform a AJAX search
+                            data: function (params) {
+                                return {
+                                    q: params.term, // search query
+                                    action: "lkppgetcateg" // AJAX action for admin-ajax.php
+                                };
+                            },
+                            processResults: function( data ) {
+                                var options = [];
+                                if ( data ) {
+                                    // data is the array of arrays, and each of them contains ID and the Label of the option
+                                    $.each( data, function( index, text ) { 
+                                        // do not forget that "index" is just auto incremented value
+                                        options.push( { id: text[0], text: text[1]  } );
+                                    });
+                                }
+                                return {
+                                    results: options
+                                };
+                            },
+                            cache: true
+                        },
+                        minimumInputLength: 3 // the minimum of symbols to input before perform a search
+                    });
+        
+                    // multiple select with AJAX search
+                    $("#lkpp_brand_id").select2({
+                        ajax: {
+                            url: ajaxurl, // AJAX URL is predefined in WordPress admin
+                            dataType: "json",
+                            delay: 250, // delay in ms while typing when to perform a AJAX search
+                            data: function (params) {
+                                return {
+                                    q: params.term, // search query
+                                    action: "lkppgetbrand" // AJAX action for admin-ajax.php
+                                };
+                            },
+                            processResults: function( data ) {
+                                var options = [];
+                                if ( data ) {
+                                    // data is the array of arrays, and each of them contains ID and the Label of the option
+                                    $.each( data, function( index, text ) { // do not forget that "index" is just auto incremented value
+                                        options.push( { id: text[0], text: text[1]  } );
+                                    });
+                                }
+                                return {
+                                    results: options
+                                };
+                            },
+                            cache: true
+                        },
+                        minimumInputLength: 3 // the minimum of symbols to input before perform a search
+                    });
+        
+                    // render datepicker
+                    $(".lkpp_expired_date").datepicker({
+                        dateFormat : "yy-mm-dd"
+                    });
+    
+                    // Hide or Show tkdn field
+                    $("#local_product").change(function() {
+                        $(".tkdn_field").hide();
+                        if ($(this).val() == "yes") {
+                            $(".tkdn_field").show();
+                        }
+                    }).change();
+    
+                    // Calculate LKPP Disc Percentage when LKPP price changed
+                    $("#lkpp_price").change(function() {
+                        $lkpp_price_inc = $(this).val();
+                        $web_price = $("#_regular_price").val();
+                        $lkpp_price_exc = Math.round($lkpp_price_inc - ($lkpp_price_inc * (10/100)));
+                        $lkpp_disc = 100 - Math.round(($lkpp_price_exc/$web_price) * 100);
+                        document.getElementById("lkpp_disc").value = $lkpp_disc; 
+                    }).change();
+    
+                    // Calculate LKPP Disc Percentage when Web price changed
+                    $("#_regular_price").change(function() {
+                        $web_price = $(this).val();
+                        $lkpp_price_inc = $("#lkpp_price").val();
+                        $lkpp_price_exc = Math.round($lkpp_price_inc - ($lkpp_price_inc * (10/100)));
+                        $lkpp_disc = 100 - Math.round(($lkpp_price_exc/$web_price) * 100);
+                        document.getElementById("lkpp_disc").value = $lkpp_disc;  
+                    }).change();';
+        if ( function_exists('wc_enqueue_js') ) {
+            wc_enqueue_js( $inline );
+        } else {
+            $woocommerce->add_inline_js( $inline );
+        } */
 	
 	    // Note the 'id' attribute needs to match the 'target' parameter set above
         ?>
         <div id='lkpp_data_options' class='panel woocommerce_options_panel'>
-            <?php ?>
             <div class='options_group'>
                 <?php
 
@@ -386,7 +480,6 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 
         </div>
         <?php
-
     }
 
     /**
@@ -495,7 +588,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
     function admin_scripts(){
      
         // please create also an empty JS file in your theme directory and include it too
-        wp_enqueue_script('lkpp_admin', 'https://stage.spakat.id/wp-content/plugins/woocommerce-lkpp-v5-connector-0.0.2_1/assets/js/admin.js', array( 'jquery', 'select2' ) ); 
+        wp_enqueue_script('lkpp_admin', plugin_dir_url( __FILE__ ) . 'assets/js/admin.js', array( 'jquery', 'select2' ) ); 
      
     }
 
